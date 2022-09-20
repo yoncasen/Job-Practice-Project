6fa54c71b59da591dae71fc96c214ee2f15935ac
yonca <sen.yoncaa@gmail.com>
2022-09-20 14:41:01 +0300

    Navigate to Login
    Login navigation for unauthorized access

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,19 +1,28 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux';
+import { Navigate } from "react-router-dom";
 
 import CompanyList from './CompanyList'
 
 const MainPage = () => {
 
-  return (
-    <div>
-      
-      <div className='container bg-white p-4 mt-5'>
-        <CompanyList/>
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn)
+
+
+  if (!isLoggedIn) {
+
+    return <Navigate replace to="/" />;
+
+  } else {
+
+    return (
+      <div>
+        <div className='container bg-white p-4 mt-5'>
+          <CompanyList/>
+        </div>
       </div>
-      
-    </div>
-  )
+    )
+  }
 }
 
 export default MainPage
